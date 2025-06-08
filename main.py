d4de9f7171f970b497039dda465eba618378624b
Pranav3782 <97328773+Pranav3782@users.noreply.github.com>
2025-06-08 18:17:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["https://pranav3782.github.io/SPA_front/"],
+    allow_origins=["*"],
 )
 
 llm = ChatGroq(
